952f51019cddfbb38c7881f97dcb4e6f66c0f5a1
Marie Gandrille <mgandrille@yahoo.fr>
2020-04-28 10:07:33 +0200

    Exercice1 finalisé

---
+++ partials/jumbotron.php
@@ -0,0 +1,10 @@
+<section class="jumbotron text-center">
+            <div class="container">
+                <h1>Web Force 3 - Zoo</h1>
+                <p class="lead text-muted">Bienvenue dans le nouveau Zoo créé par la génialissime équipe 2020 de Web Force 3 !</p>
+                <p>
+                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
+                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+                </p>
+            </div>
+        </section>
